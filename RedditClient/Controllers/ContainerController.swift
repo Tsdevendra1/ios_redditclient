c8f973569b698302bbc3e869450cf28de2eebe10
Tharuka Devendra <tsdevendra1@gmail.com>
2019-08-05 19:25:56 +0100

    Initial file setup

--- RedditClient/Controllers/ContainerController.swift
+++ RedditClient/Controllers/ContainerController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ContainerController.swift
 //  RedditClient
 //
 //  Created by Tharuka Devendra on 05/08/2019.
@@ -8,11 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ContainerController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
---
+++ RedditClient/Controllers/HomeController.swift
@@ -0,0 +1,17 @@
+//
+// Created by Tharuka Devendra on 2019-08-05.
+// Copyright (c) 2019 Tharuka Devendra. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
