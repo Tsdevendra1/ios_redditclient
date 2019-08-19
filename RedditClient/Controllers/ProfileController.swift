43b15628513fbee44ad3f10795a5d5ac2e0f397a
Tharuka Devendra <tsdevendra1@gmail.com>
2019-08-19 23:33:17 +0100

    added custom navbar for profile view controller

--- RedditClient/Controllers/ProfileController.swift
+++ RedditClient/Controllers/ProfileController.swift
@@ -11,7 +11,46 @@ class ProfileController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
-        navigationController?.navigationBar.isHidden = true
+        navigationController?.setNavigationBarHidden(true, animated: false)
+        navigationController?.interactivePopGestureRecognizer?.delegate = nil
+        placeNavigationBar()
     }
 
+    func placeNavigationBar() {
+
+        // create navigation bar
+        let navBar = UINavigationBar()
+        navBar.delegate = self
+        navBar.barTintColor = .white
+        navBar.isTranslucent = false
+
+        view.addSubview(navBar)
+        navBar.translatesAutoresizingMaskIntoConstraints = false
+        navBar.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        navBar.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        navBar.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+
+        let item = UINavigationItem()
+        item.title = "HI"
+        let button = UIButton()
+        button.titleLabel?.text = "Back"
+        button.addTarget(self, action: #selector(dismissView), for: .touchUpInside)
+        let backBarButtton = UIBarButtonItem(customView: button)
+        item.leftBarButtonItems = [backBarButtton]
+        navBar.items = [item]
+    }
+
+    @objc func dismissView(for navBar: UINavigationItem){
+        print("hiii√")
+        self.navigationController?.popViewController(animated: true)
+    }
+
+
+}
+
+
+extension ProfileController: UINavigationBarDelegate {
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
 }
