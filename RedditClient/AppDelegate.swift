03a122601f267da4aee6779fbea8f33d3c96d63d
Tharuka Devendra <tsdevendra1@gmail.com>
2019-09-28 17:16:49 +0100

    minor change to how app delegate sets the base viewcontroller

--- RedditClient/AppDelegate.swift
+++ RedditClient/AppDelegate.swift
@@ -11,15 +11,13 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 //      Override point for customization after application launch.
-        window = UIWindow(frame: UIScreen.main.bounds)
+        let window = UIWindow(frame: UIScreen.main.bounds)
         let rootViewController = ContainerController()
-        window?.rootViewController = UINavigationController(rootViewController: rootViewController)
-        window?.makeKeyAndVisible()
+        window.rootViewController = UINavigationController(rootViewController: rootViewController)
+        window.makeKeyAndVisible()
         return true
     }
 
